af6392aa8a7c172a89ebaf41badf16ecb0b611d0
YunSeong Park <75507166+kairase024@users.noreply.github.com>
2021-03-26 17:34:44 +0900

    create navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import style from "./navbar.module.css";
+
+const Navbar = props => {
+  return (
+    <div className={style.root}>
+      <a href="#">
+        <img className={style.logo} src="./logo.png" />
+      </a>
+      <form action="" className={style.form}>
+        <input
+          className={style.input_text}
+          type="text"
+          name="search"
+          id=""
+          placeholder="enter you want"
+        />
+        <button className="button" type="submit">
+          Search
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/components/videoCard.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import style from "./videoCard.module.css";
+
+const VideoCard = props => {
+  return;
+};
+
+export default VideoCard;
